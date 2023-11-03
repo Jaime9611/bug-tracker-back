8ac2ae671c0803fcc4f5afe09f2c47153374cc74
Jaime9611 <jaimeescobar9611@gmail.com>
2023-11-03 15:09:38 -0500

    #5 Solved IT for Status and created constants util for the Endpoints

---
+++ src/main/java/com/practice/bugtracker/utils/constants/Endpoints.java
@@ -0,0 +1,10 @@
+package com.practice.bugtracker.utils.constants;
+
+public class Endpoints {
+  public static final String BASE_URL = "/api/v1";
+
+  // STATUS
+  public static final String STATUS = BASE_URL + "/status";
+  public static final String STATUS_ID = STATUS + "/{id}";
+
+}
--- src/test/java/com/practice/bugtracker/controllers/StatusControllerIT.java
+++ src/test/java/com/practice/bugtracker/controllers/StatusControllerIT.java
@@ -3,11 +3,17 @@ package com.practice.bugtracker.controllers;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import com.practice.bugtracker.dtos.StatusDTO;
+import com.practice.bugtracker.helpers.StatusBuilder;
 import com.practice.bugtracker.models.Status;
 import com.practice.bugtracker.repositories.StatusRepository;
+import jakarta.transaction.Transactional;
+import java.util.UUID;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.annotation.Rollback;
 
 @SpringBootTest
 class StatusControllerIT {
@@ -18,10 +24,28 @@ class StatusControllerIT {
   @Autowired
   StatusRepository statusRepository;
 
+  @Rollback
+  @Transactional
+  @Test
+  void shouldCreateStatusOnDB() {
+    StatusDTO dto = StatusBuilder.buildStatusDto();
+
+    ResponseEntity<?> response = statusController.createStatus(dto);
+
+    assertThat(response.getStatusCode()).isEqualTo(HttpStatus.valueOf(201));
+    assertThat(response.getHeaders().getLocation()).isNotNull();
+
+    String[] location = response.getHeaders().getLocation().getPath().split("/");
+    UUID savedId = UUID.fromString(location[4]);
+
+    Status savedStatus = statusRepository.findById(savedId).orElse(null);
+    assertThat(savedStatus).isNotNull();
+  }
+
   @Test
   void shouldReturnStatusById() {
     Status status = statusRepository.findAll().iterator().next();
-    System.out.println(status.getId());
+
     StatusDTO result = statusController.getStatusById(status.getId()).getBody();
 
     assertThat(result).isNotNull();
