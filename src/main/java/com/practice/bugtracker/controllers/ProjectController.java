31e3d59175adc895b2700fe6ebdd2902b786b4e3
Jaime9611 <jaimeescobar9611@gmail.com>
2024-07-13 13:49:04 -0500

    Added flex property to Table columns

--- src/main/java/com/practice/bugtracker/controllers/ProjectController.java
+++ src/main/java/com/practice/bugtracker/controllers/ProjectController.java
@@ -7,6 +7,7 @@ import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @RestController
 @RequiredArgsConstructor
+@CrossOrigin("http://localhost:5173")
 @RequestMapping(Endpoints.PROJECT)
 public class ProjectController {
 
